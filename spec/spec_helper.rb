7a815d6aa18046e040ed070e904584df5bd18e9f
Marcelo Goncalves <marcelocg@gmail.com>
2014-05-14 19:31:42 -0300

    Throwing generated tests away. Better RSpec configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,28 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'database_cleaner'
+require 'capybara/rspec'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-  # Add more helper methods to be used by all tests here...
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  #config.include Devise::TestHelpers, type: :controller
+  config.order = "random"
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
