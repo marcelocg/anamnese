216ffc17eac9e23dc091e91f3301995d7e2a0264
Marcelo Goncalves <marcelocg@gmail.com>
2014-07-22 22:53:05 -0300

    Interface improvements and some documentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_MSG = {
+    success: 'alert-success',
+    error: 'alert-error',
+    alert: 'alert-block',
+    notice: 'alert-success'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG[flash_type.to_sym]
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "#{bootstrap_class_for(msg_type)}") do
+        concat content_tag(:button, '×'.html_safe, class: "close", data: { dismiss: 'alert' })
+          concat message
+      end)
+    end
+    nil
+  end
 end
